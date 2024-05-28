7e09ce4f76b380929a22fb6e6e7951610a145b29
pankake <daniele_commodaro@live.it>
2024-05-28 11:39:01 +0200

    validation

--- src/main/java/com/tecnotab/mqtt/dto/input/BrightRequest.java
+++ src/main/java/com/tecnotab/mqtt/dto/input/BrightRequest.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BrightRequest extends Request {
+
+    @NotNull(message = "luxLevel cannot be null")
+    @NotBlank(message = "luxLevel cannot be empty")
     private double luxLevel;
 }
--- src/main/java/com/tecnotab/mqtt/dto/input/SoundRequest.java
+++ src/main/java/com/tecnotab/mqtt/dto/input/SoundRequest.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SoundRequest extends Request {
+
+    @NotNull(message = "decibelLevel cannot be null")
+    @NotBlank(message = "decibelLevel cannot be empty")
     private double decibelLevel;
 }
